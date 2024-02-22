ed2f6a9360e995b5d936f596d71338be1b299de6
Bjørnar <598062@stud.hvl.no>
2024-02-22 11:00:28 +0100

    oppgave a

--- src/main/java/no/hvl/dat110/messages/DeleteTopicMsg.java
+++ src/main/java/no/hvl/dat110/messages/DeleteTopicMsg.java
@@ -1,17 +1,34 @@
 package no.hvl.dat110.messages;
 
 public class DeleteTopicMsg extends Message {
+	private String topic;
 
 	// message sent from client to create topic on the broker
 
-    public DeleteTopicMsg(String user, String topic) {
-
-    }
+	public DeleteTopicMsg(String user, String topic) {
+		super(MessageType.DELETETOPIC, user);
+		this.topic = topic;
+	}
 
 	// TODO:
 	// Implement object variables - a topic is required
 
 	// Complete the constructor, get/set-methods, and toString method
 	// as described in the project text
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	@Override
+	public String toString() {
+		return "DeleteTopicMsg{" +
+		       "topic='" + topic + '\'' +
+		       "} " + super.toString();
+	}
 
 }
